457cf6c5f6cea7e69ef1e1faff53bffe69d840d4
Harry Hsu <h@stmharry.io>
2025-03-06 07:54:46 +0000

    fix: markdown-preview init issues

--- astronvim/nvim/lua/plugins/markdown-preview.lua
+++ astronvim/nvim/lua/plugins/markdown-preview.lua
@@ -1,9 +1,22 @@
+-- NOTE:
+-- snatched from https://github.com/iamcco/markdown-preview.nvim/issues/690#issuecomment-2254280534
+
+---@type LazySpec
 return {
   "iamcco/markdown-preview.nvim",
   cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
   ft = { "markdown" },
-  build = function() vim.fn["mkdp#util#install"]() end,
+  build = function(plugin)
+    if vim.fn.executable "npx" then
+      vim.cmd("!cd " .. plugin.dir .. " && cd app && npx --yes yarn install")
+    else
+      vim.cmd [[Lazy load markdown-preview.nvim]]
+      vim.fn["mkdp#util#install"]()
+    end
+  end,
   init = function()
+    if vim.fn.executable "npx" then vim.g.mkdp_filetypes = { "markdown" } end
+
     vim.g.mkdp_auto_start = 1
     vim.g.mkdp_open_to_the_world = 1
     vim.g.mkdp_port = 8080
