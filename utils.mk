1209b2f8930426ebef88e9da95b7bbfee537282a
Harry Hsu <h@stmharry.io>
2025-04-17 04:04:28 +0000

    - 🔥 (plugins): Remove activation guard in `none-ls.lua` - ♻️ (makefile): Refactor `copy-file` and `copy-dir` for better readability

--- utils.mk
+++ utils.mk
@@ -1,37 +1,37 @@
 define copy-file
-	$(eval $@_SRC := $(1))
-	$(eval $@_DST := $(2))
-	$(eval $@_LOG_PREFIX := $(3))
-	$(eval $@_FORCE := $(or $(4), false))
-
-	if [ "$($@_FORCE)" = "false" ] && [ -f "$($@_DST)" ]; then \
-		echo "$($@_LOG_PREFIX) $($@_DST) already exists, not overwriting"; \
-	else \
-		echo "$($@_LOG_PREFIX) Copying $($@_SRC) to $($@_DST)"; \
-		mkdir -p $(dir $($@_DST)) && cp $($@_SRC) $($@_DST); \
-	fi
+	@{ \
+		$(eval $@_SRC := $(1)) \
+		$(eval $@_DST := $(2)) \
+		$(eval $@_LOG_PREFIX := $(3)) \
+		$(eval $@_FORCE := $(or $(4), false)) \
+ 		\
+		if [ "$($@_FORCE)" = "false" ] && [ -f "$($@_DST)" ]; then \
+			echo "$($@_LOG_PREFIX) $($@_DST) already exists, not overwriting"; \
+		else \
+			echo "$($@_LOG_PREFIX) Copying $($@_SRC) to $($@_DST)"; \
+			mkdir -p $(dir $($@_DST)) && cp $($@_SRC) $($@_DST); \
+		fi; \
+	}
 endef
 
 define copy-dir
-	$(eval $@_SRC := $(1))
-	$(eval $@_DST := $(2))
-	$(eval $@_LOG_PREFIX := $(3))
-	$(eval $@_FORCE := $(or $(4), false))
-
-	if [ -d "$($@_DST)" ]; then \
-		if [ "$($@_FORCE)" = "true" ]; then \
-			echo "$($@_LOG_PREFIX) Removing $($@_DST)"; \
-			rm -r $($@_DST); \
-		else \
-			echo "$($@_LOG_PREFIX) $($@_DST) already exists, not overwriting"; \
-			return; \
+	@{ \
+		$(eval $@_SRC := $(1)) \
+		$(eval $@_DST := $(2)) \
+		$(eval $@_LOG_PREFIX := $(3)) \
+		$(eval $@_FORCE := $(or $(4), false)) \
+		\
+		if [ -d "$($@_DST)" ]; then \
+			if [ "$($@_FORCE)" = "true" ]; then \
+				echo "$($@_LOG_PREFIX) Removing $($@_DST)"; \
+				rm -r $($@_DST); \
+			else \
+				echo "$($@_LOG_PREFIX) $($@_DST) already exists, not overwriting"; \
+				exit 0; \
+			fi; \
 		fi; \
-	fi
-
-	if [ ! -d "$(dir $($@_DST))" ]; then \
-		mkdir -p $(dir $($@_DST)); \
-	fi
-
-	echo "$($@_LOG_PREFIX) Copying $($@_SRC) to $($@_DST)"
-	cp -r $($@_SRC) $($@_DST)
+		\
+		echo "$($@_LOG_PREFIX) Copying $($@_SRC) to $($@_DST)"; \
+		cp -r $($@_SRC) $($@_DST); \
+	}
 endef
